2b37cea6c9e3a1f640d86da3484e85f79de5d84a
swnst <247774481+swnst@users.noreply.github.com>
2026-02-18 21:51:51 +0700

    feat: add prometheus metrics endpoint

--- app/index.js
+++ app/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const client = require('prom-client');
+
 const app = express();
 const port = 3000;
 
+// collect default metrics
+client.collectDefaultMetrics();
+
 app.get('/', (req, res) => {
   res.json({ status: "ok", service: "mini-platform-app" });
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 app.listen(port, () => {
